49d85cfd4bdab46b597b2bfe4c54bcbef97b99ee
bernhardbrugger <96493137+bernhardbrugger@users.noreply.github.com>
2023-03-30 10:12:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,41 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import streamlit as st
 
-def paraphrase(text, model, tokenizer):
-    input_ids = tokenizer.encode(text, return_tensors="pt")
-    output_ids = model.generate(input_ids, max_length=256, num_return_sequences=1, no_repeat_ngram_size=3)
-    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
+def paraphrase(
+    question,
+    num_beams=5,
+    num_beam_groups=5,
+    num_return_sequences=5,
+    repetition_penalty=10.0,
+    diversity_penalty=3.0,
+    no_repeat_ngram_size=2,
+    temperature=0.7,
+    max_length=128
+):
+    input_ids = tokenizer(
+        f'paraphrase: {question}',
+        return_tensors="pt", padding="longest",
+        max_length=max_length,
+        truncation=True,
+    ).input_ids
+    
+    outputs = model.generate(
+        input_ids, temperature=temperature, repetition_penalty=repetition_penalty,
+        num_return_sequences=num_return_sequences, no_repeat_ngram_size=no_repeat_ngram_size,
+        num_beams=num_beams, num_beam_groups=num_beam_groups,
+        max_length=max_length, diversity_penalty=diversity_penalty
+    )
+
+    res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    return res
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model_name = "google/flan-t5-base"
+    device = "cpu"
+    model_name = "humarin/chatgpt_paraphraser_on_T5_base"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
     return model, tokenizer
 
 model, tokenizer = load_model()
@@ -21,10 +46,11 @@ input_text = st.text_area("Enter the text you want to paraphrase:", value="", he
 
 if st.button("Paraphrase"):
     if input_text:
-        paraphrased_text = paraphrase(input_text, model, tokenizer)
+        paraphrased_text = paraphrase(input_text)
         st.subheader("Original Text:")
         st.write(input_text)
         st.subheader("Paraphrased Text:")
-        st.write(paraphrased_text)
+        for i, p in enumerate(paraphrases):
+            st.write("Option %s: %s" % (i+1, p))
     else:
         st.warning("Please enter some text to paraphrase.")
